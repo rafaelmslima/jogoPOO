3ac79cd9025f7148134b602e72eb55384043936d
rafaelmslima <138700330+rafaelmslima@users.noreply.github.com>
2024-06-10 17:46:49 -0300

    Delete src directory

--- src/tile/Tile.java
+++
@@ -1,9 +0,0 @@
-package tile;
-
-import java.awt.image.BufferedImage;
-
-public class Tile {
-    public BufferedImage image;
-    public boolean collision = false; // criando a ideia de colisão e iniciando como falso.
-
-}
